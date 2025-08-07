474d3f93b515107194cdfe2c239cdcc0cc80fb22
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-08-07 13:55:45 -0300

    004

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
     public function nova_pagina(): View
     {
         return view('nova_pagina');
+    }
+
+    public function testes(Request $request)
+    {
+        // dados do usuário autenticado
+        // $id = auth()->user()->id;
+        // ou
+        $id = $request->user()->id;
+        echo $id;
+
+        echo '<br>';
+
+        // $username = auth()->user()->name;
+        // ou
+        $username = $request->user()->name;
+        echo $username;
+    }
+
+    public function nova_pagina_publica(): View
+    {
+        return view('nova_pagina_publica');
+    }
+
+    public function main_logout()
+    {
+        // fazer o logout sem POST - limpar o usuario da sessão
+        Auth::logout();
+
+        // invalidar a sessão e regenerar o token
+        session()->invalidate();
+        session()->regenerateToken();
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/nova_pagina_publica', [MainController::class, 'nova_pagina_publica'])->name('nova_pagina_publica');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -21,6 +23,7 @@ Route::middleware('auth')->group(function () {
 // nossas routes
 Route::middleware(['auth'])->group(function () {
     Route::get('/nova_pagina', [MainController::class, 'nova_pagina'])->name('nova_pagina');
+    Route::get('/testes', [MainController::class, 'testes'])->name('testes');
 });
 // Route::get('/nova_pagina', [MainController::class, 'nova_pagina'])->name('nova_pagina')->middleware('auth'); ==> outro jeito de fazer
 
